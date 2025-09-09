c6f5c651ab551bcc51fc023ce6034699824bdaa9
C Reimers <christoph@superservice-international.com>
2025-09-09 11:12:55 +0200

    Update translation import from ugettext_lazy to gettext

--- cmsplugin_multipinmap/models.py
+++ cmsplugin_multipinmap/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext as _
 from django.template.loader import render_to_string
 from django.core.exceptions import ValidationError
 from django.conf import settings
